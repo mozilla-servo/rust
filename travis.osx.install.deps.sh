9922b30dba5ab676067996356175dbb8927fda02
Lars Bergstrom <lars@lars.com>
2014-06-24 14:46:39 -0500

    Updates for multi-target and cached LLVM

--- travis.osx.install.deps.sh
+++ travis.osx.install.deps.sh
@@ -2,5 +2,5 @@ brew update
 brew install gcc
 #wget http://llvm.org/releases/3.4.1/clang+llvm-3.4.1-x86_64-apple-darwin10.9.tar.xz
 #tar zxvf clang+llvm-3.4.1-x86_64-apple-darwin10.9.tar.xz
-wget http://servo-rust.s3.amazonaws.com/llvm-for-rustc/llvm-for-rustc.tgz
-tar zxvf llvm-for-rustc.tgz
+wget http://servo-rust.s3.amazonaws.com/llvm-for-rustc/llvm-for-rustc-osx.tgz
+tar zxvf llvm-for-rustc-osx.tgz
