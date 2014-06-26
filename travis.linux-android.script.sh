e8a0cba364abfc3786d6b43fc8eb7799a81f2a7a
Lars Bergstrom <lars@lars.com>
2014-06-26 14:35:18 -0500

    Rename dist/ file on android to be unique

--- travis.linux-android.script.sh
+++ travis.linux-android.script.sh
@@ -1,3 +1,1 @@
-make -j4 rustc-stage1 TIME_PASSES=1
-make -j4 rustc-stage2 TIME_PASSES=1
-make -j4 dist TIME_PASSES=1
+make -j4 rustc-stage1 TIME_PASSES=1 && make -j4 rustc-stage2 TIME_PASSES=1 && make -j4 dist TIME_PASSES=1 && mv dist/rust-0.11.0-pre-x86_64-unknown-linux-gnu.tar.gz dist/rust-0.11.0-pre-x86_64-unknown-linux-gnu-android.tar.gz
