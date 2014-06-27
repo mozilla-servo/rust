ef50e8a47228165b121f48ff5491407836f16ace
Lars Bergstrom <lars@lars.com>
2014-06-27 16:06:10 -0500

    Dummy work for incomplete platform for pass. Fix .script file to work inside the script it gets pasted into.

--- travis.linux-android.script.sh
+++ travis.linux-android.script.sh
@@ -1,1 +1,4 @@
-make -j4 rustc-stage1 TIME_PASSES=1 && make -j4 rustc-stage2 TIME_PASSES=1 && make -j4 dist TIME_PASSES=1 && mv dist/rust-0.11.0-pre-x86_64-unknown-linux-gnu.tar.gz dist/rust-0.11.0-pre-x86_64-unknown-linux-gnu-android.tar.gz
+make -j4 rustc-stage1 TIME_PASSES=1
+make -j4 rustc-stage2 TIME_PASSES=1
+make -j4 dist TIME_PASSES=1
+mv dist/rust-0.11.0-pre-x86_64-unknown-linux-gnu.tar.gz dist/rust-0.11.0-pre-x86_64-unknown-linux-gnu-android.tar.gz
