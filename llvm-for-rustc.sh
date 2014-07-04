08734be3c8530d16853f9d592255cd47f07edb36
Lars Bergstrom <lars@lars.com>
2014-07-03 19:00:05 -0500

    Fixup for osx generation

--- llvm-for-rustc.sh
+++ llvm-for-rustc.sh
@@ -4,7 +4,7 @@ mkdir llvm-for-rustc/bin
 mkdir llvm-for-rustc/include
 mkdir llvm-for-rustc/lib
 cp -r $FLAVOR/llvm/include/ llvm-for-rustc/include/
-cp -r $FLAVOR/llvm/Release+Asserts/bin llvm-for-rustc/bin
-cp -r $FLAVOR/llvm/Release+Asserts/lib llvm-for-rustc/lib
+cp -r $FLAVOR/llvm/Release+Asserts/bin/ llvm-for-rustc/bin/
+cp -r $FLAVOR/llvm/Release+Asserts/lib/ llvm-for-rustc/lib/
 cp -r src/llvm/include/ llvm-for-rustc/include/
 tar zcvf llvm-for-rustc.tgz llvm-for-rustc
